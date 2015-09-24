1207f38b1c8d17f1199302de6c980cd2996a9a7b
latoxico <ricsjp@gmail.com>
2015-09-24 22:48:24 +0000

    exercicio 22-09

--- blogTads/teste.rb
+++ blogTads/teste.rb
@@ -1,1 +1,24 @@
-puts "dovia"
+class Conta 
+    attr_accessor :numero
+    attr_accessor :conta
+    attr_accessor :agencia
+    attr_accessor :banco
+    attr_accessor :saldo
+    attr_accessor :limite
+    attr_accessor :nome_usuario
+    
+    def sacar(quantidade)
+       self.saldo = self.saldo - quantidade
+    end
+end
+
+a = new Conta
+
+a.nome_usuario = "eu"
+a.saldo = 150
+
+puts "Correntista #{a.nome_usuario} possui saldo #{a.saldo}"
+
+a.sacar(100)
+
+puts "novo saldo é #{a.saldo}"
